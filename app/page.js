e1b9307bc1cfa0c9635951a884adc3ccd697f0a9
experiencei <ayelojahighbee01@gmail.com>
2024-05-01 10:16:47 +0100

    working on supabase integration with prisma orm

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
+{/* <LoginLink>Sign in</LoginLink>
+<RegisterLink>Sign up</RegisterLink> */}
 
 export default function Home() {
   return (
